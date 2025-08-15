8646cd1b24fd828824cd4709c34ae3909bb243b9
Eduardo Barros <eduardoscbarros@hotmail.com>
2025-08-15 16:13:07 -0300

    feat: implemented root endpoint

--- src/app.controller.spec.ts
+++ src/app.controller.spec.ts
@@ -15,8 +15,10 @@ describe('AppController', () => {
   });
 
   describe('root', () => {
-    it('should return "Hello World!"', () => {
-      expect(appController.getHello()).toBe('Hello World!');
+    it('should return {"message": "English Dictionary"}', () => {
+      expect(appController.getHello()).toEqual({
+        message: 'English Dictionary',
+      });
     });
   });
 });
